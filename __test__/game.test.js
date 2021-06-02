17b8ee9ba04b83cb87d81ee24197e14cde7491ac
Ramesan PP <84701649+RamesanPP@users.noreply.github.com>
2021-06-02 11:28:50 +0530

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,10 +2,11 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async () => {
+  it('Contains the compiled JavaScript', async (done) => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
+      done()
     })
   })
 })
